f5f017f87da781d40e0871633901cb4d3f324216
vovchif17 <vovchif17@gmail.com>
2021-02-16 15:00:42 -0500

    Add footer, header and controllers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def full_title(page_title = '')
     if page_title.empty?
-      base_title
-    else
+      base_title = "Ruby on Rails Tutorial Sample App"
+        else
       page_title + " | " + base_title
     end
   end
